d1aaf63bb8d13abcfe9a8e3f392e92c023a57f5c
fernandodossantos001 <fernandodossantos001@gmail.com>
2019-05-26 00:49:11 -0300

    IotNac

--- app/src/main/java/br/com/fiap/componentes/naciot/IotPojo.java
+++ app/src/main/java/br/com/fiap/componentes/naciot/IotPojo.java
@@ -2,5 +2,60 @@ package br.com.fiap.componentes.naciot;
 
 public class IotPojo {
     private int id;
+    private int hum;
+    private int luz;
+    private int buzzer;
+    private int led;
 
+    public IotPojo(int id, int hum, int luz, int buzzer, int led) {
+        this.id = id;
+        this.hum = hum;
+        this.luz = luz;
+        this.buzzer = buzzer;
+        this.led = led;
+    }
+
+    public IotPojo(){
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getHum() {
+        return hum;
+    }
+
+    public void setHum(int hum) {
+        this.hum = hum;
+    }
+
+    public int getLuz() {
+        return luz;
+    }
+
+    public void setLuz(int luz) {
+        this.luz = luz;
+    }
+
+    public int getBuzzer() {
+        return buzzer;
+    }
+
+    public void setBuzzer(int buzzer) {
+        this.buzzer = buzzer;
+    }
+
+    public int getLed() {
+        return led;
+    }
+
+    public void setLed(int led) {
+        this.led = led;
+    }
 }
